eaa5baa921c921c6bb6f0512e8fbc620f71e8a72
Clint Jang <jang.wangsu@gmail.com>
2018-06-05 19:18:35 +0900

    🔧 modify

--- sample/src/essental/print/fmtPrint.go
+++ sample/src/essental/print/fmtPrint.go
@@ -2,14 +2,31 @@ package main
 
 import "fmt"
 
-func main() {
+// Print 함수의 응용, 간격 벌리기
+func printSpacing(title string) {
+	fmt.Println("")
+	fmt.Println("-----------------------------")
+	fmt.Printf("--- %s\n", title)
+	fmt.Println("-----------------------------")
+}
 
+func testPrintAndPrinffAndPrintln() {
 	fmt.Print("print")
 	fmt.Printf("printf")
 	fmt.Println("println")
 	fmt.Print("print")
 	fmt.Printf("-%d-printf\n", 1)
 	fmt.Println("println")
+}
+
+func main() {
+
+	printSpacing("Print, Prinff, Println")
+
+	// Print, Prinff, Println
+	testPrintAndPrinffAndPrintln()
+
+	printSpacing("string")
 
 	var kor string = "한글"
 	var eng string = "english"
@@ -26,12 +43,16 @@ func main() {
 	fmt.Println(eng)
 	fmt.Println(jp)
 
+	printSpacing("bool")
+
 	// bool
 	fmt.Println(jp == jpSame)
 	fmt.Println(isBool)
 	fmt.Println(!isBool)
 	fmt.Println(isBool == isBoolSame)
 	fmt.Println(intValue > intValueSame)
+
+	printSpacing("int")
 
 	// int
 	fmt.Println(intValue)
@@ -44,9 +65,13 @@ func main() {
 	// --intValue	// error
 	// ++intValue	// error
 
+	printSpacing("문자열 붙이기")
+
 	// 문자열 붙이기
 	fmt.Println(kor + eng + jp)
 	fmt.Println(kor + "," + eng + "," + jp)
+
+	printSpacing("제어문자")
 
 	// 제어문자
 	fmt.Println("경고음\a실행되나?")
